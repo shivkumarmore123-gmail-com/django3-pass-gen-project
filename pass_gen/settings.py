cdfad4370b78915568b98fcce654feee72a288ab
shivcode <shivkumarmore123@gmail.com>
2021-05-24 00:59:22 +0530

    I made some tweek

--- pass_gen/settings.py
+++ pass_gen/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u(1v)(-fk!z@m2khdbo333mt=)#0v0nj^%b#((ket-&1i%yccq'
+SECRET_KEY = 'u(1v)(-fk!z@m2khdbo333mt=)fgxjlk#0v0nj^%b#((ket-&1i%yccq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
